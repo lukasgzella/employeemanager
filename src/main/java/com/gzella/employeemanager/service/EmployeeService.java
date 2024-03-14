8365f7b947ced7f9c1c8b3530c6151cb2f7c803b
lukas.gzella <lukas.gzella@gmail.com>
2024-03-14 22:22:18 +0100

    implement EmployeeService

---
+++ src/main/java/com/gzella/employeemanager/service/EmployeeService.java
@@ -0,0 +1,42 @@
+package com.gzella.employeemanager.service;
+
+import com.gzella.employeemanager.model.Employee;
+import com.gzella.employeemanager.repo.EmployeeRepo;
+import com.gzella.employeemanager.exception.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo ;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + " is not found"));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
